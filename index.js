425443e09bc3dfc6eba121b68e266128e1853d9e
elizabethlosotua <elizabethlosotua@gmail.com>
2022-09-29 13:20:05 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const employee = {
+    name: Sam,
+    streecAddress:  '1Broadway'
+};
+function updateEmpolyeeWithkenyaandValue(employee, key, values) {
+    return Object.assign({}, employee, {[key]: values});
+};
+function destructivelyUpdateEmployeewithkeyvalue(employee, key, values) {
+    employee[key] = values
+    return employee;
+}
+function deletedFromEmployeebykey(employee, key) {
+    const newobj = {...employee}
+    delete newobj[key];
+    return newobj;
+}
+function destructivelyDeleteFromEmployeeBykey(employee, key, values){
+    delete employee[key];
+    return employee;
+}
